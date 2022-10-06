8c51d7e7167714ede571f8981792246eed2b5d90
Petri Bayley <petribayley@gmail.com>
2022-10-06 23:46:45 +1100

    Dashboard cards

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../styling/main.css';
+import '../styling/dashboard.css';
+
+var test_data = {
+  "servers": [
+    {
+      'name': 'survival',
+      'users_connected': 5,
+      'max_users': 10
+    },
+    {
+      'name': 'creative',
+      'users_connected': 20,
+      'max_users': 30
+    }
+  ]
+};
 
 function Dashboard() {
   return (
-    <>
-    </>
+    <div className="dashboard-div">
+    {test_data.servers.map((element) => {
+      return (
+        <div className="dashboard-div-card">
+          <h2>{element.name}</h2>
+        </div>
+      )
+    })}
+    </div>
   )
 }
 

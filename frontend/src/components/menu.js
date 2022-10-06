b4462356090a603cc31df8ddaf3ddaca6cf0ed7f
Petri Bayley <petribayley@gmail.com>
2022-10-06 22:44:21 +1100

    Fixes to typos

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import "../styling/menu.css"
-import minemon_logo from "../styling/minemon_logo.png"
+import mineman_logo from "../styling/mineman_logo.png"
 
 const menu_select = async (e) => {
   console.log('Menu item selected: ' + e.target.id);
@@ -10,7 +10,7 @@ const menu_select = async (e) => {
 function Menu() {
   return (
     <div className="navigation-div">
-        <img className="navigation-div-logo" src={minemon_logo} alt="Minemon Logo"/>
+        <img className="navigation-div-logo" src={mineman_logo} alt="Minemon Logo"/>
         <button className="navigation-div-button" id="dashboard" onClick={menu_select}>Dashboard</button>
         <button className="navigation-div-button" id="create_server" onClick={menu_select}>Create Server</button>
         <button className="navigation-div-button" id="global_config" onClick={menu_select}>Global Config</button>
